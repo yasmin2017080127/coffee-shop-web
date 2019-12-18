75326fbdab611e1310ab6f563584a7c45e6a9c00
yasmin2017080127 <56387559+yasmin2017080127@users.noreply.github.com>
2019-12-18 12:48:00 +0800

    Add files via upload

---
+++ contact-form-script.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+    $('.submit').click(function (event) {
+        console.log('clicked button')
+
+        var name = $('.name').val()
+        var email = $('.email').val()
+        var phone = $('.phone').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+
+        if(name.length >= 2) {
+            statusElm.append('<div>name is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>name is not valid</div>') 
+        }
+        
+        if(email.length > 5 && email.includes('@') && email.includes('.')) {
+            statusElm.append('<div>Email is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Email is not valid</div>') 
+        }
+
+        if(phone.length >= 2) {
+            statusElm.append('<div>Subject is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>subject is not valid</div>') 
+        }
+        if(message.length > 10) {
+            statusElm.append('<div>Message is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Message is not valid</div>')
+        }
+    })
+})
